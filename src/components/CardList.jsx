c8bbbe377a4f2fce76b70d7c98521fadf07a4f33
Divanshu096 <divanshu.bakshi@slu.edu>
2024-12-01 19:11:46 +0000

    Completed lab06

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,74 @@
-const CardList = () => {
-  return ();
-}
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import Button from "./Button";
+import Search from "./Search"
+
+
+const limit = 10;
+
+const CardList = ({ data }) => {
+  const [offset, setOffset] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setProducts(data.slice(offset, offset + limit));
+    }
+  }, [offset, data]);
+
+  const filterTags = (tagQuery) =>{
+    const filtered = data.filter(product =>{
+      if(!tagQuery){
+        return product
+      }
+
+      return product.tags.find(({title}) => title === tagQuery)
+    })
+
+    setOffset(0)
+    setProducts(filtered)
+  }
+
+  const handlePrevious = () => {
+    setOffset((prevOffset) => Math.max(prevOffset - limit, 0));
+  };
+
+  const handleNext = () => {
+    setOffset((prevOffset) =>
+      prevOffset + limit < data.length ? prevOffset + limit : prevOffset
+    );
+  };
+
+  return (
+    <div className="cf pa2">
+      <Search handleSearch={filterTags}/>
+      <div className="mt2 mb2">
+        {products && products.length > 0 ? (
+          products.map((product) => (
+            <Card key={product.id} {...product} />
+          ))
+        ) : (
+          <p>No products available.</p>
+        )}
+      </div>
+
+      <div className="flex items-center justify-center pa4">
+        <Button
+          text="Previous"
+          handleClick={() => setOffset(offset - limit)}
+          disabled={offset === 0}
+        />
+          
+        
+        <Button
+          text="Next"
+          handleClick={() => setOffset(offset + limit)}
+          disabled={offset + limit >= data.length}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default CardList;
+
